74a09e0f2f0920dd3b8a2446be308373ea1b2ec9
Shantanu <shantanu.kumar.singh26@gmail.com>
2026-07-05 23:15:47 +0530

    Minor changes added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='w-full fixed bottom-0 bg-[black] text-white flex justify-between md:justify-center md:gap-4 items-center items-center h-[60px]'>
        <h2 className='text-xl gap-1 cursor-default flex items-center'><img src="logo.png" width={30} alt="" />LockIN</h2>
 
-       <span className='flex'>Made with <img className='mx-2' src="heart.png"  width={30}  alt="" /> by Shantanu</span> 
+       <span className='flex'>Made with <img className='mx-0.5 md:mx-2' src="heart.png"  width={30}  alt="" /> by <a href="https://myportfolio-one-topaz-21.vercel.app/" className='font-normal mx-1 text-blue-600 hover:underline' target='_'> Shantanu</a></span> 
 
      
       
